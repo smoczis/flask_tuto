c55afa25aea0cebf17107773f6abb72a2ddefbe3
Romanowski Mateusz <rb26049G@raiffeisen.pl>
2019-02-27 16:00:35 +0100

    Copyt files onto git

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or "ROMEUSZ"
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+                              'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    POSTS_PER_PAGE = 5
+    LANGUAGES = ['en', 'pl']
